fd713f4f7945bef3cf3ea6826c9dcb3340469d15
Dave Howcroft <dave.howcroft@gmail.com>
2018-09-19 15:39:11 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='hpy_utilities',
+      version='0.1',
+      description='HapPY Utilities for getting things done in python.',
+      author='David M. Howcroft',
+      author_email='dave.howcroft@gmail.com',
+      url='https://github.com/dmhowcroft/hpy_utilities',
+      packages=['hpy_utilities'],
+     )
